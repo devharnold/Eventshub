c0e2d1dfe309b5d867f460abbc87dd76c3527894
Henry Arnold <devharnold@gmail.com>
2025-08-24 03:30:08 +0300

    Debug BookingController class

--- eventhub/src/main/java/com/eventhub/events/controller/BookingController.java
+++ eventhub/src/main/java/com/eventhub/events/controller/BookingController.java
@@ -1,19 +1,13 @@
 package com.eventhub.events.controller;
 
-import com.eventhub.events.model.Booking;
 import com.eventhub.events.model.BookingRequest;
 import com.eventhub.events.dto.TicketDTO;
 import com.eventhub.events.model.Ticket;
 import com.eventhub.events.service.BookingService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.eventhub.events.service.PaymentFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/bookings")
@@ -29,13 +23,18 @@ public class BookingController {
     public ResponseEntity<TicketDTO> bookTicket(@RequestBody BookingRequest request) {
         try {
             Ticket ticket = bookingService.bookTicket(request);
+
             TicketDTO ticketDTO = new TicketDTO(
                     ticket.getTicketNumber(),
                     ticket.getUsername(),
+                    ticket.getEventDate(),
                     ticket.getEventName(),
                     ticket.getOrganizationName(),
-                    ticket.getEventDate()
+                    ticket.getQrCodeBase64()
             );
+
+            return ResponseEntity.ok(ticketDTO);
+
         } catch (PaymentFailedException e) {
             return ResponseEntity.status(HttpStatus.PAYMENT_REQUIRED).build();
         } catch (Exception e) {
